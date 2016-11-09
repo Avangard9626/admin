6ea63a33431c309bf991b28066ccbe6f5e03205c
Avangard9626 <sharupov.96@yandex.ru>
2016-11-09 16:59:53 +0300

    123

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
-var trs = [];
+var trs = []
+var forms = []
 function open_add() {
     hide_all()
     $('#table-form').show()
@@ -11,25 +12,45 @@ function close_add() {
 
 function open_change() {
     hide_all()
-    trs = $(".table input:checkbox:checked").map(function(){
+    $("thead input:checkbox:checked").prop("disabled", true)
+    trs = $("tbody input:checkbox:checked").map(function(){
         return $(this).parent().parent()
     }).get();
     trs.forEach(function(item, i, arr){
-        $(item).addClass('selected')
-        form = $('#table-form').show()
-        form
+        form = $('#table-form').clone()
+        $(form).show()
+        id = $(item).find('input:checkbox:first').prop('value')
+        $(form).prop('id', 'table-form-' + id)
+        $(form).find('#id').text(id)
+        tds = $(item).find('td').get()
+        $(form).find('#add-topic').val($(tds[2]).text())
+        $(form).find('#add-text').val($(tds[3]).text())
+        forms.push(form)
         form = $(item).replaceWith(form)
     })
-    console.log(trs)
     $('#change-controls').show()
 }
 
 function accept_change() {
     hide_all()
-    console.log(trs)
-    trs.forEach(function(item, i, arr){
-        $(item).val().removeClass('selected')
+    forms.forEach(function(form, i, arr){
+        tr = trs[i]
+        $(form).replaceWith(tr)
     })
+    trs = []
+    forms = []
+    $("thead input:checkbox:checked").prop("disabled", false)
+}
+
+function decline_change() {
+    hide_all()
+    forms.forEach(function(form, i, arr){
+        tr = trs[i]
+        $(form).replaceWith(tr)
+    })
+    trs = []
+    forms = []
+    $("thead input:checkbox:checked").prop("disabled", false)
 }
 
 function open_remove() {
@@ -49,3 +70,13 @@ function hide_all() {
 function trigger_checks(elem) {
     $(':checkbox').prop( "checked", $(elem).prop('checked') );
 }
+
+function scroll_bottom() {
+  $("html, body").animate({ scrollTop: $(document).height()-$(window).height() }, "fast");
+  return false;
+};
+
+function scroll_top() {
+  $("html, body").animate({ scrollTop: 0}, "fast");
+  return false;
+};
